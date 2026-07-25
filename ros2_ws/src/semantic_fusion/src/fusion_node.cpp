93bf36db0cfd16b02961189e2832a587e4c49985
Samarthraman01 <samarthghanate540@gmail.com>
2026-07-25 16:17:37 +0200

    depth image subscriber added to fusion node

--- ros2_ws/src/semantic_fusion/src/fusion_node.cpp
+++ ros2_ws/src/semantic_fusion/src/fusion_node.cpp
@@ -7,9 +7,21 @@ public:
   FusionNode(): Node("fusion_node")
   {
     RCLCPP_INFO(this->get_logger(), "Fusion node has started");
+    depth_sub = this->create_subscription<sensor_msgs::msg::Image>(
+                "/camera/depth/image_raw", 
+                10,
+                std::bind(&FusionNode::depth_callback, this, std::placeholders::_1)
+    );
+    RCLCPP_INFO(this->get_logger(), "Waiting for depth images....");
   }
 private:
-
+  void depth_callback(const sensor_msgs::msg::Image::SharedPtr msg)
+  {
+    RCLCPP_INFO(this->get_logger(), "depth image recieved - wdith: %d height: %d",
+                                    msg->width,
+                                    msg->height);
+  }
+  rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr depth_sub;
 };
 
 int main(int argc, char*argv[])
