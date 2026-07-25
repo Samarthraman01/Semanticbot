de5b7e2235cb531a1804cc2fc078d07cd63c5243
Samarthraman01 <samarthghanate540@gmail.com>
2026-07-25 16:17:39 +0200

    inference_node: ONNX Runtime integrated, SegFormer-B2 loads successfully in C++ ROS 2 node

--- ros2_ws/src/segformer_inference/src/inference_node.cpp
+++ ros2_ws/src/segformer_inference/src/inference_node.cpp
@@ -3,28 +3,37 @@
 #include <vector>
 #include <memory>
 #include <chrono>
+#include <onnxruntime_cxx_api.h>
 
 using namespace std;
 
 class inference_node : public rclcpp::Node {
 public:
-    inference_node() : Node("inference_node") {
-    image_sub_ = create_subscription<sensor_msgs::msg::Image>(
-        "/camera/image/raw", 10,
-        [this](const sensor_msgs::msg::Image::SharedPtr msg) {
-            image_callback(msg);
-        }
-    );
-    RCLCPP_INFO(this->get_logger(), "Inference node has been started.");
-}
-
-private:
-    void image_callback(const sensor_msgs::msg::Image::SharedPtr image_msg){
-        RCLCPP_INFO(get_logger(), "recieved image with: %d x %d" , image_msg->width, image_msg->height);
+    inference_node() : Node("inference_node"),
+        env_(ORT_LOGGING_LEVEL_WARNING, "inference"),
+        session_(env_,
+                 "/home/samarthws/Semanticbot/models/segformer_b2.onnx",
+                 Ort::SessionOptions{})
+    {
+        image_sub_ = create_subscription<sensor_msgs::msg::Image>(
+            "/camera/image/raw", 10,
+            [this](const sensor_msgs::msg::Image::SharedPtr msg) {
+                image_callback(msg);
+            }
+        );
+        RCLCPP_INFO(get_logger(), "SegFormer model loaded!");
+        RCLCPP_INFO(get_logger(), "Inference node started!");
     }
 
-    //member viariables
+private:
+    void image_callback(const sensor_msgs::msg::Image::SharedPtr image_msg) {
+        RCLCPP_INFO(get_logger(), "received image: %d x %d",
+                    image_msg->width, image_msg->height);
+    }
+
     rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr image_sub_;
+    Ort::Env env_;
+    Ort::Session session_;
 };
 
 int main(int argc, char* argv[]) {
